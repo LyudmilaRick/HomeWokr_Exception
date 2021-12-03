9f63e297cf3f56545abfa66f724b6a5705a6098a
RickLyudmila <Lrick@yandex.ru>
2021-12-03 20:24:02 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/homewokr_exception/HomeWokrExceptionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homewokr_exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWokrExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWokrExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/homewokr_exception/HomeWokrExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homewokr_exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWokrExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
